63097bdc3150bb661336e73b6f2eb922e4c96c8c
ChiranjeeviShinde <chiranjeevishinde2007@gmail.com>
2025-09-09 11:53:20 +0530

    Create View Page

--- models/camp.js
+++ models/camp.js
@@ -3,22 +3,22 @@ const mongoose = require("mongoose");
 const campSchema = new mongoose.Schema({
   name: {
     type: String,
-    // required: true,
+    required: true,
   },
   location: {
     type: String,
-    //required: true,
+    required: true,
   },
   image: {
     type: String,
   },
   description: {
     type: String,
-    //required: true,
+    required: true,
   },
   price: {
     type: Number,
-    //required: true,
+    required: true,
     min: 0,
   },
 });
