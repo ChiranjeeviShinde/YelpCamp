999049dca00db097b4484d480163f082d654fc5f
Dikshant Namdeo <dikshant.namdeo@gmail.com>
2025-09-23 23:11:42 +0530

    Fetch Reviews to GET /camp/:id

--- index.js
+++ index.js
@@ -1,83 +1,88 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const mongoose = require("mongoose");
-const methodOverride = require("method-override");
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
-const Camp = require("./models/camp");
+const Camp = require('./models/camp');
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/YelpCamp")
+  .connect('mongodb://127.0.0.1:27017/YelpCamp')
   .then(() => {
-    console.log("MongoDB connected");
+    console.log('MongoDB connected');
   })
-  .catch((err) => {
-    console.log("MongoDB connection error:", err);
+  .catch(err => {
+    console.log('MongoDB connection error:', err);
   });
 
 const reviewSchema = new mongoose.Schema({
-    review: String
-})
+  review: String,
+});
 
-const Review = mongoose.model("Review", reviewSchema);
+const Review = mongoose.model('Review', reviewSchema);
 
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
-app.get("/camps/home", (req, res) => {
-  res.render("camps/home");
+app.get('/camps/home', (req, res) => {
+  res.render('camps/home');
 });
 
-app.get("/camps", async (req, res) => {
+app.get('/camps', async (req, res) => {
   const camps = await Camp.find({});
-  res.render("camps/index", { camps });
+  res.render('camps/index', { camps });
 });
 
-app.get("/camps/new", (req, res) => {
-  res.render("camps/new");
+app.get('/camps/new', (req, res) => {
+  res.render('camps/new');
 });
 
 app.get('/camps/:id', async (req, res) => {
-    const { id } = req.params;
-    const camp = await Camp.findById(id)
-    res.render('camps/show', { camp })
-})
+  const { id } = req.params;
+  const camp = await Camp.findById(id);
+  const reviews = await Review.find({});
+  console.log('Reviews for camp:', camp._id, reviews);
+  res.render('camps/show', { camp, reviews });
+});
 
 app.post('/camps', async (req, res) => {
-    const newCamp = new Camp(req.body);
-    await newCamp.save();
-    res.redirect(`/camps/${newCamp._id}`)
-})
+  const newCamp = new Camp(req.body);
+  await newCamp.save();
+  res.redirect(`/camps/${newCamp._id}`);
+});
 
 app.get('/camps/:id/edit', async (req, res) => {
-    const { id } = req.params;
-    const camp = await Camp.findById(id);
-    res.render('camps/edit', { camp })
-})
+  const { id } = req.params;
+  const camp = await Camp.findById(id);
+  res.render('camps/edit', { camp });
+});
 
 app.put('/camps/:id', async (req, res) => {
-    const { id } = req.params;
-    const camp = await Camp.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
-    res.redirect(`/camps/${camp._id}`);
-})
+  const { id } = req.params;
+  const camp = await Camp.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/camps/${camp._id}`);
+});
 
 app.delete('/camps/:id', async (req, res) => {
-    const { id } = req.params;
-    const deletedCamp = await Camp.findByIdAndDelete(id);
-    res.redirect('/camps');
-})
+  const { id } = req.params;
+  const deletedCamp = await Camp.findByIdAndDelete(id);
+  res.redirect('/camps');
+});
 
 app.post('/camps/:id', async (req, res) => {
-    const { id } = req.params;
-    const review = new Review(req.body);
-    await review.save();
-    const camp = await Camp.findById(id)
-    res.render('camps/show', { camp, review })
-})
+  const { id } = req.params;
+  const review = new Review(req.body);
+  await review.save();
+  const camp = await Camp.findById(id);
+  res.render('camps/show', { camp, review });
+});
 
 app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log('Server is running on http://localhost:3000');
 });
