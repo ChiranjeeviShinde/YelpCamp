bb813560b4be51f677c85d0ddf6bfc3ab6cca65e
ChiranjeeviShinde <chiranjeevishinde2007@gmail.com>
2025-10-11 16:20:12 +0530

    Delete Review

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
-const Camp = require('./models/camp');
+const Camp = require("./models/camp");
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/YelpCamp')
+  .connect("mongodb://127.0.0.1:27017/YelpCamp")
   .then(() => {
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   })
-  .catch(err => {
-    console.log('MongoDB connection error:', err);
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
   });
 
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 
-app.get('/camps/home', (req, res) => {
-  res.render('camps/home');
+app.get("/", (req, res) => {
+  res.render("camps/home");
 });
 
-app.get('/camps', async (req, res) => {
+app.get("/camps", async (req, res) => {
   const camps = await Camp.find({});
-  res.render('camps/index', { camps });
+  res.render("camps/index", { camps });
 });
 
-app.get('/camps/new', (req, res) => {
-  res.render('camps/new');
+app.get("/camps/new", (req, res) => {
+  res.render("camps/new");
 });
 
-app.get('/camps/:id', async (req, res) => {
+app.get("/camps/signin", (req, res) => {
+  res.render("camps/auth");
+});
+
+app.get("/camps/:id", async (req, res) => {
   const { id } = req.params;
   const camp = await Camp.findById(id);
-  res.render('camps/show', { camp });
+  res.render("camps/show", { camp });
 });
 
-app.post('/camps', async (req, res) => {
+app.post("/camps", async (req, res) => {
   const newCamp = new Camp(req.body);
   newCamp.reviews = [];
   await newCamp.save();
@@ -48,13 +52,13 @@ app.post('/camps', async (req, res) => {
   res.redirect(`/camps/${newCamp._id}`);
 });
 
-app.get('/camps/:id/edit', async (req, res) => {
+app.get("/camps/:id/edit", async (req, res) => {
   const { id } = req.params;
   const camp = await Camp.findById(id);
-  res.render('camps/edit', { camp });
+  res.render("camps/edit", { camp });
 });
 
-app.put('/camps/:id', async (req, res) => {
+app.put("/camps/:id", async (req, res) => {
   const { id } = req.params;
   const camp = await Camp.findByIdAndUpdate(id, req.body, {
     runValidators: true,
@@ -63,20 +67,31 @@ app.put('/camps/:id', async (req, res) => {
   res.redirect(`/camps/${camp._id}`);
 });
 
-app.delete('/camps/:id', async (req, res) => {
+app.delete("/camps/:id", async (req, res) => {
   const { id } = req.params;
   const deletedCamp = await Camp.findByIdAndDelete(id);
-  res.redirect('/camps');
+  res.redirect("/camps");
 });
 
-app.post('/camps/:id', async (req, res) => {
+app.post("/camps/:id", async (req, res) => {
   const { id } = req.params;
   const camp = await Camp.findById(id);
   camp.reviews.push(req.body);
   await camp.save();
-  res.render('camps/show', { camp });
+  res.render("camps/show", { camp });
+});
+
+app.delete("/camps/:id/review/:rid", async (req, res) => {
+  const { id, rid } = req.params;
+  const camp = await Camp.findById(id);
+  await Camp.findByIdAndUpdate(
+    id,
+    { $pull: { reviews: { _id: rid } } },
+    { new: true }
+  );
+  res.redirect(`/camps/${camp.id}`);
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
